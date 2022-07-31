bbf9970129b71b0a38c403d840517afc901f7372
Raphael <raphael.fruehwirth@student.tugraz.at>
2022-07-31 14:59:17 +0200

    get img links from db

---
+++ classifier/dataDownloader.py
@@ -0,0 +1,21 @@
+import sqlite3 as sql   
+import urllib.request
+#urllib.request.urlretrieve("http://www.digimouth.com/news/media/2011/09/google-logo.jpg", "local-filename.jpg")
+
+
+def getCarData():
+    links = []
+    with sql.connect("../car.db") as conn:
+        cur = conn.cursor()
+        cur.execute("SELECT title, image_urls FROM car")
+        rows = cur.fetchall()
+        for row in rows:
+            links.append([row[0], [x for x in row[1].split(";") if x != ""]])   
+    return links
+
+def main():
+    data = getCarData()
+    print()
+
+if __name__ == "__main__":
+    main()
